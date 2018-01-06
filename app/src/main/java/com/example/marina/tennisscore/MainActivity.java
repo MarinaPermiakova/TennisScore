2f1948bca78d8689350db29bcdf2b5bee6b10dbe
marina <marina.mk.glazov@gmail.com>
2018-01-06 12:26:28 +0400

    marina.mk.glazov@gmail.com

--- app/src/main/java/com/example/marina/tennisscore/MainActivity.java
+++ app/src/main/java/com/example/marina/tennisscore/MainActivity.java
@@ -14,34 +14,81 @@ public class MainActivity extends AppCompatActivity {
     // Tracks the score for Player 2
     int scorePlayerTwo = 0;
 
+    // Show the score for Players
+    TextView scoreViewFirst;
+    TextView scoreViewSecond;
+
+    // Plus points for Players
+    Button plusButtonFirst;
+    Button plusButtonSecond;
+
+    Button resetButton;
+
+
+
+    static final String STATE_SCOREPLAYER1 = "scorePlayerFirst";
+    static final String STATE_SCOREPLAYER2 = "scorePlayerSecond";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Disable resetButton
-        Button resetButton = (Button) findViewById(R.id.reset_button);
+        resetButton = findViewById(R.id.reset_button);
         resetButton.setEnabled(false);
+
+        scoreViewFirst = findViewById(R.id.score_player_one);
+        scoreViewSecond = findViewById(R.id.score_player_two);
+
+        plusButtonFirst = findViewById(R.id.button_player_one);
+        plusButtonSecond = findViewById(R.id.button_player_two);
+
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the player's current score state
+        savedInstanceState.putInt(STATE_SCOREPLAYER1, scorePlayerOne);
+        savedInstanceState.putInt(STATE_SCOREPLAYER2, scorePlayerTwo);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore state members from saved instance
+        scorePlayerOne = savedInstanceState.getInt(STATE_SCOREPLAYER1);
+        scorePlayerTwo = savedInstanceState.getInt(STATE_SCOREPLAYER2);
+
+        score1(scorePlayerOne);
+        score2(scorePlayerTwo);
+    }
+
+
+    /**
+     * Increase the score for Player 1.
+     */
+    public void plusPointOne(View view) {
+        scorePlayerOne++;
+        score1(scorePlayerOne);
     }
 
     /**
-     * Increase the score for Player 1 and show current result of game in TextViews.
+     * Show current result of game in TextViews.
      */
-
-    public void plusPointOne(View view) {
-
-        // Increase the score for Player 1
-        scorePlayerOne++;
-
-        // TextViews for current result of game
-        TextView scoreView = (TextView) findViewById(R.id.score_player_one);
-        TextView scoreViewTwo = (TextView) findViewById(R.id.score_player_two);
+    public int score1(int Int) {
 
         // first point for Player 1
-        if (scorePlayerOne == 1) scoreView.setText("15");
+        if (scorePlayerOne == 1) scoreViewFirst.setText("15");
 
         // second point for Player 1
-        if (scorePlayerOne == 2) scoreView.setText("30");
+        if (scorePlayerOne == 2) scoreViewFirst.setText("30");
 
         // third point for Player 1
         if (scorePlayerOne == 3)
@@ -49,9 +96,9 @@ public class MainActivity extends AppCompatActivity {
         // compare result with Player 2: 40 or deuce
         {
             if (scorePlayerOne == scorePlayerTwo) {
-                scoreView.setText("De");
-                scoreViewTwo.setText("De");
-            } else scoreView.setText("40");
+                scoreViewFirst.setText("De");
+                scoreViewSecond.setText("De");
+            } else scoreViewFirst.setText("40");
         }
 
         // forth point for Player 1
@@ -59,48 +106,52 @@ public class MainActivity extends AppCompatActivity {
 
             // compare result with Player 2: deuce or Ad (advantage) or win
             if (scorePlayerOne == scorePlayerTwo) {
-                scoreView.setText("De");
-                scoreViewTwo.setText("De");
+                scoreViewFirst.setText("De");
+                scoreViewSecond.setText("De");
             }
             if ((scorePlayerTwo >= 3) & (scorePlayerOne - scorePlayerTwo == 1)) {
-                scoreView.setText("Ad");
-                scoreViewTwo.setText("");
+                scoreViewFirst.setText("Ad");
+                scoreViewSecond.setText("");
             }
             if ((scorePlayerTwo <= 2) | (scorePlayerOne - scorePlayerTwo == 2)) {
-                scoreView.setText("Game");
-                scoreViewTwo.setText("");
-                printWinnerText();
+                scoreViewFirst.setText("WIN");
+                scoreViewSecond.setText("");
+                plusButtonFirst.setEnabled(false);
+                plusButtonSecond.setEnabled(false);
+                resetButton.setEnabled(true);
             }
         }
+        return (scorePlayerOne);
     }
+
 
     /**
      * Increase the score for Player 2 and show current result of game in TextViews.
      */
-
     public void plusPointTwo(View view) {
-
-        // Increase the score for Player 1
         scorePlayerTwo++;
+        score2(scorePlayerTwo);
+    }
 
-        // TextViews for current result of game
-        TextView scoreView = (TextView) findViewById(R.id.score_player_one);
-        TextView scoreViewTwo = (TextView) findViewById(R.id.score_player_two);
 
+    /**
+     * show current result of game in TextViews.
+     */
+    public int score2(int Int) {
         // first point for Player 2
-        if (scorePlayerTwo == 1) scoreViewTwo.setText("15");
+        if (scorePlayerTwo == 1) scoreViewSecond.setText("15");
 
         // second point for Player 2
-        if (scorePlayerTwo == 2) scoreViewTwo.setText("30");
+        if (scorePlayerTwo == 2) scoreViewSecond.setText("30");
 
         // third point for Player 2
         if (scorePlayerTwo == 3) {
 
             // compare result with Player 2: 40 or deuce
             if (scorePlayerTwo == scorePlayerOne) {
-                scoreView.setText("De");
-                scoreViewTwo.setText("De");
-            } else scoreViewTwo.setText("40");
+                scoreViewFirst.setText("De");
+                scoreViewSecond.setText("De");
+            } else scoreViewSecond.setText("40");
         }
 
         // Fourth point for Player 2
@@ -108,72 +159,38 @@ public class MainActivity extends AppCompatActivity {
 
             // compare result with Player 1: deuce or Ad (advantage)
             if (scorePlayerOne == scorePlayerTwo) {
-                scoreView.setText("De");
-                scoreViewTwo.setText("De");
+                scoreViewFirst.setText("De");
+                scoreViewSecond.setText("De");
             }
 
             // compare result with Player 1: deuce or Ad (advantage) or win
             if ((scorePlayerOne >= 3) & (scorePlayerTwo - scorePlayerOne == 1)) {
-                scoreViewTwo.setText("Ad");
-                scoreView.setText("");
+                scoreViewSecond.setText("Ad");
+                scoreViewFirst.setText("");
             }
             if ((scorePlayerOne <= 2) | (scorePlayerTwo - scorePlayerOne == 2)) {
-                scoreViewTwo.setText("Game");
-                scoreView.setText("");
-                printWinnerText();
+                scoreViewSecond.setText("WIN");
+                scoreViewFirst.setText("");
+                plusButtonFirst.setEnabled(false);
+                plusButtonSecond.setEnabled(false);
+                resetButton.setEnabled(true);
             }
         }
+        return (scorePlayerTwo);
     }
 
-    /**
-     * Displays the result of game and score of the game.
+     /**
+     * reset variables, textViews and Buttons to initial state
      */
-
-    public void printWinnerText() {
-        TextView winnerView = (TextView) findViewById(R.id.win_view);
-        if (scorePlayerOne > scorePlayerTwo){
-            winnerView.setText("Player 1 win game!");
-        }
-        else winnerView.setText("Player 2 win game!");
-
-       // Disable buttons for score and enable resetButton
-
-        Button plusButtonFirst = (Button) findViewById(R.id.button_player_one);
-        plusButtonFirst.setEnabled(false);
-
-        Button plusButtonSecond = (Button) findViewById(R.id.button_player_two);
-        plusButtonSecond.setEnabled(false);
-
-        Button resetButton = (Button) findViewById(R.id.reset_button);
-        resetButton.setEnabled(true);
-    }
-
-    /**
-     *  reset variables, textViews and Buttons to initial state
-     */
-
     public void resetClick(View view) {
-
 
         scorePlayerOne = 0;
         scorePlayerTwo = 0;
 
-        TextView scoreViewFirst = (TextView) findViewById(R.id.score_player_one);
         scoreViewFirst.setText(R.string._0);
-
-        TextView scoreViewSecond = (TextView) findViewById(R.id.score_player_two);
         scoreViewSecond.setText(R.string._0);
-
-        TextView winnerView = (TextView) findViewById(R.id.win_view);
-        winnerView.setText("");
-
-        Button plusButtonFirst = (Button) findViewById(R.id.button_player_one);
         plusButtonFirst.setEnabled(true);
-
-        Button plusButtonSecond = (Button) findViewById(R.id.button_player_two);
         plusButtonSecond.setEnabled(true);
-
-        Button resetButton = (Button) findViewById(R.id.reset_button);
         resetButton.setEnabled(false);
 
     }
